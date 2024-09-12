16069b89e78104919bc2055de7f9c6e5b10258f7
Phan Anh Phuong <phuong95646@donga.edu.vn>
2024-09-12 11:21:16 +0700

    baiTap3

--- baitap1/src/main/java/Nhom1/donga/baitap1/TrangChuController.java
+++ baitap1/src/main/java/Nhom1/donga/baitap1/TrangChuController.java
@@ -3,6 +3,8 @@ package Nhom1.donga.baitap1;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TrangChuController {
@@ -16,5 +18,34 @@ public class TrangChuController {
     public String chiTiet(Model model) {
         model.addAttribute("user", "Phan Anh Phuong");
         return "user";
+    }
+
+    @GetMapping("/addUser")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "addUser";
+    }
+
+    @PostMapping("/addUser")
+    public void saveUser(@ModelAttribute("User")User user) {
+        System.out.println("FirstName: " + user.getFirstName());
+        System.out.println("LastName: " + user.getLastName());
+    }
+
+    @GetMapping("/addUserCccd")
+    public String addUserCccd(Model model) {
+        model.addAttribute("userCccd", new UserCccd());
+        return "addUserCccd";
+    }
+
+    @PostMapping("/addUserCccd")
+    public void saveUserCccd(@ModelAttribute("UserCccd")UserCccd userCccd) {
+        System.out.println("So: " + userCccd.getNumber());
+        System.out.println("Ho va ten: " + userCccd.getFullName());
+        System.out.println("Ngay sinh: " + userCccd.getDateOfBirth());
+        System.out.println("Quoc Tich: " + userCccd.getNationality());
+        System.out.println("Gioi tinh: " + userCccd.getSex());
+        System.out.println("Que Quan: " + userCccd.getPlaceOfOrigin());
+        System.out.println("Noi thuong tru: " + userCccd.getPlaceOfResidence());
     }
 }
--- baitap1/src/main/java/Nhom1/donga/baitap1/UserCccd.java
+++ baitap1/src/main/java/Nhom1/donga/baitap1/UserCccd.java
@@ -1,4 +1,76 @@
 package Nhom1.donga.baitap1;
 
+import java.time.LocalDate;
+
 public class UserCccd {
+    private int number;
+    private String fullName;
+    private LocalDate dateOfBirth;
+    private Gender sex;
+    private String nationality;
+    private String placeOfOrigin;
+    private String placeOfResidence;
+
+    public UserCccd() {
+        this.number = number;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.sex = sex;
+        this.nationality = nationality;
+        this.placeOfOrigin = placeOfOrigin;
+        this.placeOfResidence = placeOfResidence;
+    }
+
+    public enum Gender {
+        MALE, FEMALE, OTHER
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Gender getSex(){
+        return sex;
+    }
+    public void setSex(Gender sex){
+        this.sex = sex;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPlaceOfOrigin() {
+        return placeOfOrigin;
+    }
+    public void setPlaceOfOrigin(String placeOfOrigin) {
+        this.placeOfOrigin = placeOfOrigin;
+    }
+
+    public String getPlaceOfResidence() {
+        return placeOfResidence;
+    }
+    public void setPlaceOfResidence(String placeOfResidence) {
+        this.placeOfResidence = placeOfResidence;
+    }
 }
